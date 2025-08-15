b003682898a3cd612f010d87c2609989a0e03465
Brayteh <ya3koub5@gmail.com>
2025-08-15 12:50:19 +0200

    profilePage: Input_Validation

--- dr_time/lib/screens/ProfilePage.dart
+++ dr_time/lib/screens/ProfilePage.dart
@@ -1,89 +1,147 @@
 import 'package:dr_time/screens/sign_in_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+
+  final _formKey = GlobalKey<FormState>();
+  final _fNameCtrl = TextEditingController();
+  final _lNameCtrl = TextEditingController();
+  final _dbirthCtrl = TextEditingController();
+
+    @override
+    void dispose(){
+      _fNameCtrl.dispose();
+      _lNameCtrl.dispose();
+      _dbirthCtrl.dispose();
+      super.dispose();
+    }
+
+  void _submit() {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            "Done!",
+          ),
+        ),
+      );
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(height: 99,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 16),
-                child: Text("Profile",style:TextStyle(fontSize: 32,fontWeight: FontWeight.bold),  ),   //profile
-                ),
-                Padding(padding: EdgeInsets.only(right: 16),
-                child: IconButton(
-                  icon: Icon(Icons.power_settings_new,color: Colors.red,size: 33,),
-                  onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context)=> LogInPage()));},
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(12),
+          children: [
+            SizedBox(height: 99,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Text("Profile",style:TextStyle(fontSize: 32,fontWeight: FontWeight.bold),  ),   //profile
                   ),
-                  ),
-            ],
-          ),
-          SizedBox(height: 25,),
-          Padding(
-            padding: const EdgeInsets.only(left: 16),
-            child: Text("First name",style: TextStyle(fontSize: 22),), // first name
-          ),
-          Padding(padding: EdgeInsets.all(16),   //Border
-          child: TextField( keyboardType: TextInputType.name,
-            decoration: InputDecoration(
-              hintText: "your name",
+                  Padding(padding: EdgeInsets.only(right: 16),
+                  child: IconButton(
+                    icon: Icon(Icons.power_settings_new,color: Colors.red,size: 33,),
+                    onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context)=> LogInPage()));},
+                    ),
+                    ),
+              ],
             ),
-          ),
-          ),
-          SizedBox(height: 15,),
-          Padding(padding: EdgeInsets.only(left: 16),
-          child: Text("Last name",style: TextStyle(fontSize: 22),),  //last name
-          ),
-             Padding(padding: EdgeInsets.all(16),   //Border
-          child: TextField( keyboardType: TextInputType.name,
-            decoration: InputDecoration(
-              hintText: "your last name",
+            SizedBox(height: 25,),
+            Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Text("First name",style: TextStyle(fontSize: 22),), // first name
             ),
-          ),
-          ),
-          SizedBox(height: 15,),
-          Padding(padding: EdgeInsets.only(left: 16),
-          child: Text("Date of birth",style: TextStyle(fontSize: 22),),   //date of birth
-          ),
-          Padding(padding: EdgeInsets.all(16),
-          child: TextField(
-            keyboardType: TextInputType.datetime,
-            decoration: InputDecoration(
-              hintText: "your date of birth",
+            Padding(padding: EdgeInsets.all(16), 
+              //Border
+            child: TextFormField(
+              validator: (value){
+                final v = value?.trim() ?? "";
+                if (v.isEmpty) return "please write your name";
+                if (v.length < 3) return "please write a valid name";
+                return null;
+              },
+               keyboardType: TextInputType.name,
+              decoration: InputDecoration(
+                hintText: "your name",
+              ),
             ),
-          ),
-          ),
-          SizedBox(height: 15,),
-          Padding(padding: EdgeInsetsGeometry.only(left: 16),
-          child: Text("Disease name",style: TextStyle(fontSize: 22),),
-          ),
-          Padding(padding: EdgeInsets.all(16),
-          child: TextField( keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              hintText: "your disease name",
             ),
+            SizedBox(height: 15,),
+            Padding(padding: EdgeInsets.only(left: 16),
+            child: Text("Last name",style: TextStyle(fontSize: 22),),  //last name
+            ),
+               Padding(padding: EdgeInsets.all(16),   //Border
+            child: TextFormField(
+              validator: (value){
+                final v = value?.trim() ?? "";
+                if (v.isEmpty) return "please write your last name";
+                if (v.length < 3) return "please write a valid last name";
+                return null; 
+              },
+               keyboardType: TextInputType.name,
+              decoration: InputDecoration(
+                hintText: "your last name",
+              ),
+            ),
+            ),
+            SizedBox(height: 15,),
+            Padding(padding: EdgeInsets.only(left: 16),
+            child: Text("Date of birth",style: TextStyle(fontSize: 22),),   //date of birth
+            ),
+            Padding(padding: EdgeInsets.all(16),
+            child: TextFormField(
+              validator: (value) {
+                final v = value?.trim() ?? "";
+                if (v.isEmpty) return "please enter your date of birth";
+                if (!RegExp(r'^\d{2}\.\d{2}\.\d{4}$').hasMatch(v)) return "the form must be dd.mm.yyyy";
+                return null;
+              },
+              keyboardType: TextInputType.text,
+              inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'[0-9.]')),],
+              decoration: InputDecoration(
+                hintText: "your date of birth",
+              ),
+            ),
+            ),
+            SizedBox(height: 15,),
+            Padding(padding: EdgeInsetsGeometry.only(left: 16),
+            child: Text("Disease name",style: TextStyle(fontSize: 22),),
+            ),
+            Padding(padding: EdgeInsets.all(16),
+            child: TextField( keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                hintText: "your disease name",
+              ),
+            ),
+            ),
+                    Center(                                                      // Save button 
+                child: SizedBox(
+                width: 200, // العرض المطلوب
+                child: ElevatedButton(
+                style: ElevatedButton.styleFrom(elevation: 2,backgroundColor: const Color.fromARGB(255, 68, 202, 232)),
+                onPressed: _submit,
+                child:
+                   Text("Save",style:TextStyle(fontSize: 23,fontWeight: FontWeight.bold, color: Colors.white),),
+            ),
+           ),
           ),
-          ),
-                  Center(                                                      // Save button 
-              child: SizedBox(
-              width: 200, // العرض المطلوب
-              child: ElevatedButton(
-              style: ElevatedButton.styleFrom(elevation: 2,backgroundColor: const Color.fromARGB(255, 68, 202, 232)),
-              onPressed: () {},
-              child:
-                 Text("Save",style:TextStyle(fontSize: 23,fontWeight: FontWeight.bold, color: Colors.white),),
-          ),
-         ),
+          ],
         ),
-        ],
       ),
     );
   }
--- dr_time/lib/screens/sign_in_screen.dart
+++ dr_time/lib/screens/sign_in_screen.dart
@@ -60,77 +60,79 @@ class _LogInPageState extends State<LogInPage> {
   Widget build(BuildContext context) {
 
     return  Scaffold(
-              body: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset("images/med1.jpg"), // sura lfo2 
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: EdgeInsets.only(left: 16),
-                    child: Text("Sign in", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(height: 50),
-            Padding(padding: EdgeInsets.only(left: 16),
-            child: Text("Email", style: TextStyle(fontSize: 15), // Email
-            ),
-            ),
-                  SizedBox(height: 3),
-                  Padding(padding: EdgeInsets.all(16),  
-              child: TextField(
-                controller: _emailController,      //hay l7ta t5tfe error lma nktb
-                onChanged: (text){
-                  if(_emailErrorText != null){setState(() {
-                    _emailErrorText = null;
-                  });}
-                },
-                 keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  hintText: "enter your Email"), // from Theme "Border"
-              ) 
-            ),
-                 Padding(padding: EdgeInsets.only(left: 16),
-                 child: Text("Password", style: TextStyle(fontSize: 15), // Password
-            ),
-            ),
-                 SizedBox(height: 3),
-                 Padding(padding: EdgeInsets.all(16),                   
-                 child: TextField(
-                  controller: _passwordController,
-                  obscureText: true, keyboardType: TextInputType.visiblePassword,
-                  decoration: InputDecoration(
-                    errorText: _emailErrorText,  // la 3rd error massage
-                    hintText: "enter your Password"),),// hyda border from Theme kamen                                   // hyda child 5asso bel border tafasil
-
-            ),
-            SizedBox(height: 50),
-            Center(   // sign in button 
-              child: SizedBox(
-              width: 200, // العرض المطلوب
-              child: ElevatedButton(
-              style: ElevatedButton.styleFrom(elevation: 2,backgroundColor: const Color.fromARGB(255, 68, 202, 232)),
-              onPressed: _login, // Navi zu HomePage ist oben, call methode lta7a2o2   
-              child:
-                 Text("Sign In",style:TextStyle(fontSize: 23,fontWeight: FontWeight.bold, color: Colors.white),),
-          ),
-         ),
-        ),
-            SizedBox(height: 15),
-            Center(
-              child: TextButton( // you dont have..?
-                style: TextButton.styleFrom(foregroundColor: Colors.blue), // لون النص
-                onPressed: (){Navigator.push(context,MaterialPageRoute(builder: (context)=> SignUpPage()), // Navi zu signUp
-                );},
-                 child: Text("you don't have an account? sign up")),
-            ),
-              Center(
-              child: TextButton( // forget pass
-                style: TextButton.styleFrom(foregroundColor: Colors.blue), // لون النص
-                onPressed: (){}, child: Text("Forget password"),
+              body: Form(
+                child: ListView(
+                padding: const EdgeInsets.all(0),
+                children: [
+                  Image.asset("images/med1.jpg"), // sura lfo2 
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: EdgeInsets.only(left: 16),
+                      child: Text("Sign in", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),
                 ),
+                            ),
+                            SizedBox(height: 50),
+                            Padding(padding: EdgeInsets.only(left: 16),
+                            child: Text("Email", style: TextStyle(fontSize: 15), // Email
+                            ),
+                            ),
+                    SizedBox(height: 3),
+                    Padding(padding: EdgeInsets.all(16),  
+                child: TextField(
+                  controller: _emailController,      //hay l7ta t5tfe error lma nktb
+                  onChanged: (text){
+                    if(_emailErrorText != null){setState(() {
+                      _emailErrorText = null;
+                    });}
+                  },
+                   keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: "enter your Email"), // from Theme "Border"
+                ) 
+                            ),
+                   Padding(padding: EdgeInsets.only(left: 16),
+                   child: Text("Password", style: TextStyle(fontSize: 15), // Password
+                            ),
+                            ),
+                   SizedBox(height: 3),
+                   Padding(padding: EdgeInsets.all(16),                   
+                   child: TextField(
+                    controller: _passwordController,
+                    obscureText: true, keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      errorText: _emailErrorText,  // la 3rd error massage
+                      hintText: "enter your Password"),),// hyda border from Theme kamen                                   // hyda child 5asso bel border tafasil
+                
+                            ),
+                            SizedBox(height: 50),
+                            Center(   // sign in button 
+                child: SizedBox(
+                width: 200, // العرض المطلوب
+                child: ElevatedButton(
+                style: ElevatedButton.styleFrom(elevation: 2,backgroundColor: const Color.fromARGB(255, 68, 202, 232)),
+                onPressed: _login, // Navi zu HomePage ist oben, call methode lta7a2o2   
+                child:
+                   Text("Sign In",style:TextStyle(fontSize: 23,fontWeight: FontWeight.bold, color: Colors.white),),
+                          ),
+                         ),
+                        ),
+                            SizedBox(height: 15),
+                            Center(
+                child: TextButton( // you dont have..?
+                  style: TextButton.styleFrom(foregroundColor: Colors.blue), // لون النص
+                  onPressed: (){Navigator.push(context,MaterialPageRoute(builder: (context)=> SignUpPage()), // Navi zu signUp
+                  );},
+                   child: Text("you don't have an account? sign up")),
+                            ),
+                Center(
+                child: TextButton( // forget pass
+                  style: TextButton.styleFrom(foregroundColor: Colors.blue), // لون النص
+                  onPressed: (){}, child: Text("Forget password"),
+                  ),
+                ),
+                          ],
+                        ),
               ),
-          ],
-        ),
     );
   }
 }
